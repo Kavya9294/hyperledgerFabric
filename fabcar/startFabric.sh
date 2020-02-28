739875ac78f4b3a5865eaef1fcb770a1d4c8fa3a
Kavya9294 <kavya.ub9294@gmail.com>
2020-02-28 16:19:20 -0500

    Added the following config since the10 peers did not work Config- 5 orderers from 2 organizations 15 peers under each organization

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -47,21 +47,21 @@ else
 fi
 
 
-# clean the keystore
-#rm -rf ./hfc-key-store
+# # clean the keystore
+# rm -rf ./hfc-key-store
 
-# launch network; create channel and join peer to channel
-pushd ../first-network
-#echo y | ./byfn.sh down
-echo y | ./byfn.sh up -a -n -s couchdb
-popd
+# # launch network; create channel and join peer to channel
+# pushd ../first-network/addPeers5
+# echo y | ./byfn.sh down
+# echo y | ./byfn.sh up -a -n -s couchdb
+# popd
 
 CONFIG_ROOT=/opt/gopath/src/github.com/hyperledger/fabric/peer
 ORG1_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 ORG1_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 ORG2_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 ORG2_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-ORDERER_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+ORDERER_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/ordererOrganizations/ord1.example.com/orderers/orderer1.ord1.example.com/msp/tlscacerts/tlsca.ord1.example.com-cert.pem
 
 PEER0_ORG1="docker exec
 -e CORE_PEER_LOCALMSPID=Org1MSP
@@ -72,7 +72,7 @@ cli
 peer
 --tls=true
 --cafile=${ORDERER_TLS_ROOTCERT_FILE}
---orderer=orderer.example.com:7050"
+--orderer=orderer1.ord1.example.com:7050"
 
 PEER1_ORG1="docker exec
 -e CORE_PEER_LOCALMSPID=Org1MSP
@@ -83,7 +83,7 @@ cli
 peer
 --tls=true
 --cafile=${ORDERER_TLS_ROOTCERT_FILE}
---orderer=orderer.example.com:7050"
+--orderer=orderer1.ord1.example.com:7050"
 
 PEER0_ORG2="docker exec
 -e CORE_PEER_LOCALMSPID=Org2MSP
@@ -94,7 +94,7 @@ cli
 peer
 --tls=true
 --cafile=${ORDERER_TLS_ROOTCERT_FILE}
---orderer=orderer.example.com:7050"
+--orderer=orderer1.ord1.example.com:7050"
 
 PEER1_ORG2="docker exec
 -e CORE_PEER_LOCALMSPID=Org2MSP
@@ -105,7 +105,7 @@ cli
 peer
 --tls=true
 --cafile=${ORDERER_TLS_ROOTCERT_FILE}
---orderer=orderer.example.com:7050"
+--orderer=orderer1.ord1.example.com:7050"
 
 echo "Packaging smart contract on peer0.org1.example.com"
 ${PEER0_ORG1} lifecycle chaincode package \
