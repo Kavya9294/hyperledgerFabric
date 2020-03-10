88d1ab638231c68187edaf7781cbb084fd54e943
Kavya9294 <kavya.ub9294@gmail.com>
2020-03-09 21:35:49 -0400

    Made the 10 orderers and 4 peers of each org

---
+++ first-network/orderers20/parseExcel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+docker logs orderer1.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer2.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer3.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer4.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer5.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer6.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer7.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer8.ord1.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+
+docker logs orderer1.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer2.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer3.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer4.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer5.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer6.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer7.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+docker logs orderer8.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
+
+cat sample.txt | awk '{print($2)}' >> sample2.txt
+cat sample.txt | awk '{print($5)}' >> sample3.txt
+cat sample.txt | awk '{print substr($0,length,1)}' >> sample4.txt
+
+cut -d' ' -f1,2,3,4,5,6,7,8 --complement sample.txt | sed 's/node=.*//g' | sed 's/,//g'>> sample5.txt
+
+paste -d, sample2.txt sample3.txt sample4.txt sample5.txt > sample1.txt
+sed  -i '1i Time, Operation, Node, Description' sample1.txt
+cat sample1.txt > config20Orderer.xlsx
