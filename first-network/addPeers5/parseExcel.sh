073f33630ca8bed47618309c56051e2ac4127fa1
Kavya9294 <kavya.ub9294@gmail.com>
2020-03-18 23:51:51 -0400

    Made these changes for 6 orderers config

--- first-network/addPeers5/parseExcel.sh
+++ first-network/addPeers5/parseExcel.sh
@@ -7,6 +7,22 @@ docker logs orderer1.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep 
 docker logs orderer2.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
 docker logs orderer3.ord2.example.com 2>&1 | grep orderer.consensus.etcd | grep -v byfn-sys-channel >> sample.txt
 
+docker logs peer0.org1.example.com >& peer01.txt | sed -e '1,9d' < peer01.txt >>temp_peer0.txt
+docker logs peer1.org1.example.com >& peer11.txt | sed -e '1,9d' < peer11.txt >>temp_peer0.txt
+
+docker logs peer0.org2.example.com >& peer02.txt | sed -e '1,9d' < peer02.txt >>temp_peer0.txt
+docker logs peer1.org2.example.com >& peer12.txt | sed -e '1,9d' < peer12.txt >>temp_peer0.txt
+
+cat temp_peer0.txt | awk '{print($2)}' >> peer_sample2.txt
+cat temp_peer0.txt | awk '{print($5)}' >> peer_sample3.txt
+cat temp_peer0.txt | awk '{print($4)}' >> peer_sample4.txt
+
+cut -d' ' -f1,2,3,4,5,6,7,8 --complement temp_peer0.txt | sed 's/node=.*//g' | sed 's/,//g'>> peer_sample5.txt
+
+paste -d, peer_sample2.txt peer_sample3.txt peer_sample4.txt peer_sample5.txt > peer_sample1.txt
+sed  -i '1i Time, Operation, Operation_name, Description' peer_sample1.txt
+cat peer_sample1.txt > peer_config6Orderer.csv
+
 cat sample.txt | awk '{print($2)}' >> sample2.txt
 cat sample.txt | awk '{print($5)}' >> sample3.txt
 cat sample.txt | awk '{print substr($0,length,1)}' >> sample4.txt
@@ -15,4 +31,4 @@ cut -d' ' -f1,2,3,4,5,6,7,8 --complement sample.txt | sed 's/node=.*//g' | sed '
 
 paste -d, sample2.txt sample3.txt sample4.txt sample5.txt > sample1.txt
 sed  -i '1i Time, Operation, Node, Description' sample1.txt
-cat sample1.txt > sample1.xlsx
+cat sample1.txt > config10Orderer.csv
